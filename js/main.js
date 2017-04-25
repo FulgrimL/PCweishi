a99717e9a701964b22b1fab5689b675f0f8a8fdc
Liu <pphsh@sina.com>
2017-04-25 14:29:29 +0800

    2017-4-25 14:29:13

--- js/main.js
+++ js/main.js
@@ -10,7 +10,33 @@ window.onload=function(){
 		$(".top-ul").find("li").find("em").css("display","none");
 		$(this).find("em").css("display","inline-block");
 		$(this).find("a").css("color","#fdd000");
-	})
+	});
+//	搜索页
+	
+	$(".search-control").find("li").eq(0).click(function(){
+		$(".search-control").find("li").find("span").css("color","#fff");
+		$(".search-control").find("li").find("em").css("display","none");
+		$(this).find("em").css("display","inline-block");
+		$(this).find("span").css("color","#fdd000");
+		$(".search-vedio").css("display","none");
+		$(".search-user").css("display","block");
+	});
+	$(".search-control").find("li").eq(1).click(function(){
+		$(".search-control").find("li").find("span").css("color","#fff");
+		$(".search-control").find("li").find("em").css("display","none");
+		$(this).find("em").css("display","inline-block");
+		$(this).find("span").css("color","#fdd000");
+		$(".search-vedio").css("display","block");
+		$(".search-user").css("display","none");
+	});
+	$(".search-vedio").find("ul").find("li").bind("mouseenter",function(){
+		$(this).find(".search-mask,.search-vedio-ti").slideUp();	
+	}).end().bind("mouseleave",function(){
+		$(this).find(".search-mask,.search-vedio-ti").slideDown();
+		console.log("111");
+//		$(this).find("img").slideDown();
+	});
+
 	
 	
 	

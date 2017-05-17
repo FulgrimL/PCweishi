e7069944bc28eafe9af33f370ab2802127cd47ab
Liu <pphsh@sina.com>
2017-05-17 16:43:12 +0800

    Ajax 主页实现单页面

--- js/index.js
+++ js/index.js
@@ -2,26 +2,38 @@ $(function(){
 	var liveContent = $(".live-content-total"),
 	indexContent = $(".index-content-total"),
 	vedioContent = $(".vedio-content"),
-	searchContent = $(".search-content"),
+	searchContent = $(".search-content");
+	$(".index-main").load("indexC.html .live-content-total");
+	$.getScript('../js/main.js');
 	liveLi = $(".top-ul").find("li").eq(0);
-	
 	liveLi.find("em").css("display","inline-block").end().find("a").css("color","#fdd000");
 	
-	indexContent.hide();
-	vedioContent.hide();
-	searchContent.hide();
+	indexContent.remove(); 
+	vedioContent.remove();
+	searchContent.remove();
 	$('.top-ul').find("li").bind("click",function(){
-			liveContent.hide();
-			indexContent.hide();
-			vedioContent.hide();
-			searchContent.hide();
+			liveContent.remove();
+			indexContent.remove();
+			vedioContent.remove();
+			searchContent.remove();
 		if ($(this).index() == 0) {
-			liveContent.show();
+			$(".index-main").load("indexC.html .live-content-total");
+			$.getScript('../js/main.js');
+
 		}else if($(this).index() == 1){
-			indexContent.show();
+			$(".index-main").load("indexC.html .index-content-total");
+			$.getScript('../js/main.js');
+
 		}else if($(this).index() == 2){
-			vedioContent.show();
+			$(".index-main").load("indexC.html .vedio-content-total");
+			$.getScript('../js/main.js');
+
 		}else if($(this).index() == 3){
-			searchContent.show();}
+			$(".index-main").load("indexC.html .search-content-total");
+			$.getScript('../js/main.js');
+	}
+		
+		
+		
 	})
 })
